bed36111826c5ece50f0012925c4e2a9324c1c67
linnnruoo <linruo2014@gmail.com>
2020-09-17 20:11:22 +0800

    add basic routes

---
+++ src/main/Analytics/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Analytics: React.FC<{}> = () => {
+  return (
+    <div>
+      <h1>qw qweqwe qw </h1>
+      <h1>qw qweqwe qw </h1>
+      <h1>qw qweqwe qw </h1>
+      <h1>qw qweqwe qw </h1>
+      <h1>qw qweqwe qw </h1>
+    </div>
+  )
+}
+
+export default Analytics
---
+++ src/router/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react'
+import { Route, Switch } from 'react-router'
+
+import Home from 'main/Home'
+import Analytics from 'main/Analytics'
+
+import * as routes from './routes'
+
+const AppRouter: FC<{}> = () => {
+  const loadInitialData = () => {}
+  React.useEffect(loadInitialData, [])
+
+  return (
+    <Switch>
+      <Route exact path={routes.MAIN} component={Home} />
+      <Route path={routes.ANALYTICS} component={Analytics} />
+    </Switch>
+  )
+}
+
+export default AppRouter
