cccbc03f553542db803deb5d360d6a0dfc1b8a6c
linnnruoo <linruo2014@gmail.com>
2020-09-26 19:43:07 +0800

    add socketio client

--- src/main/Analytics/RealTimeChart/index.tsx
+++ src/main/Analytics/RealTimeChart/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import io from 'socket.io-client'
 
 import Card from 'components/Card'
 import Stack from 'components/Stack'
 
+const socket = io('http://localhost:5000')
+
 const RealTimeChart: React.FC<{}> = () => {
+  const fetchData = () => {
+    socket.on('test123', () => {
+      console.log('retrieved')
+    })
+  }
+
+  React.useEffect(fetchData, [])
+
   return (
     <Stack center>
       <Card>TEST</Card>
