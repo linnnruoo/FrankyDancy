79f80879ddec2c65a5c93bba5fa38f1e10c5ae38
linnnruoo <linruo2014@gmail.com>
2020-09-29 22:51:18 +0800

    working badly

--- src/main/Analytics/RealTimeChart/ChartjsTest.tsx
+++ src/main/Analytics/RealTimeChart/ChartjsTest.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
 import 'chartjs-plugin-streaming'
+import _ from 'lodash'
+
+import * as events from 'common/events'
+import socket from 'configs/socket'
+import { getHourSecond } from 'utilities/datetime'
+import { Sensor } from 'common/models'
 
 const Chartjs: React.FC = () => {
-  const [data, setData] = React.useState([1, 2, 3, 5])
+  const startTime = Date.now()
 
+  const [sensorData, setSensorData] = React.useState<any[]>([])
+  const [rawData, setData] = React.useState<Array<{ x: string; y: number }>>([])
+
+  const [newData, setNewData] = React.useState<number>()
+
+  /**
+   * @todo: able to split into x,y,z and accelertomet / gyroscope readings
+   */
+  const fetchData = () => {
+    socket.on(events.SENSOR_INSERTION_EVENT, (sensor: Sensor) => {
+      console.log('received', sensor)
+      setNewData(sensor.accelerometer.x)
+    })
+  }
+
+  React.useEffect(fetchData, [])
+
+  console.log('re-rendered ?????')
   return (
     <Line
       type="line"
@@ -13,8 +37,16 @@ const Chartjs: React.FC = () => {
           {
             label: 'user 1',
             fill: false,
+            // data: _.map(sensorData, (sensor) => {
+            //   console.log('ARGH', sensor)
+            //   return {
+            //     y: _.get(sensor, ['accelerometer', 'y'], 0),
+            //     x: Date.now(),
+            // }
+            // }),
             data: [],
           },
+          // { label: 'user2', fill: false, data: [] },
         ],
       }}
       options={{
@@ -27,11 +59,11 @@ const Chartjs: React.FC = () => {
                   chart.data.datasets.forEach((dataset: any) => {
                     dataset.data.push({
                       x: Date.now(),
-                      y: Math.random(),
+                      y: newData,
                     })
                   })
                 },
-                delay: 1000,
+                delay: 2000,
               },
             } as any,
           ],
